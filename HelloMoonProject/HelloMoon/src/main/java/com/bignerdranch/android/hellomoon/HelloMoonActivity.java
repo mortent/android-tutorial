6383191fa74968f1ef979124244282207fed0f54
mortent <morten.j.torkildsen@gmail.com>
2013-09-29 18:45:47 -0700

    Completed chapter 13

--- HelloMoonProject/HelloMoon/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonActivity.java
+++ HelloMoonProject/HelloMoon/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonActivity.java
@@ -1,15 +1,17 @@
-package com.bignerdranch.androidhellomoon;
+package com.bignerdranch.android.hellomoon;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class HelloMoonActivity extends Activity {
+public class HelloMoonActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_moon);
+
     }
 
 
